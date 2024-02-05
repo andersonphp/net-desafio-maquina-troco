63dff4948f72fa156cdebad15b18d85b94fd8fb9
Anderson Alves <anderson.dmc07@gmail.com>
2024-02-05 10:18:31 -0300

    Concluído

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Informe o valor do produto:");
+        double valorProduto = Convert.ToDouble(Console.ReadLine()); 
+
+        Console.WriteLine("Informe o valor pago pelo cliente:");
+        double valorPago = Convert.ToDouble(Console.ReadLine());
+
+        MaquinaTroco.CalcularTroco(valorProduto, valorPago);
+    }
+}
